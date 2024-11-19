0f331fdb3792b081ab66d108a502c2dff1ce7af6
patrick soueid <soueid80@gmail.com>
2024-11-19 18:47:06 +0200

    Defined entity classes

---
+++ app/src/main/java/com/example/pennywise/data/model/SavingGoal.kt
@@ -0,0 +1,35 @@
+package com.example.pennywise.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "saving_goals",
+    foreignKeys = [ForeignKey(
+        entity = User::class,
+        parentColumns = ["userId"],
+        childColumns = ["userId"],
+        onDelete = ForeignKey.CASCADE
+    ),ForeignKey(
+        entity = Category::class,
+        parentColumns = ["categoryId"],
+        childColumns = ["categoryId"],
+        onDelete = ForeignKey.CASCADE)
+    ]
+)
+data class SavingGoal(
+    @PrimaryKey(autoGenerate = true) val goalId: Int = 0,
+    @ColumnInfo(name = "userId") val userId: Int,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "targetAmount") val targetAmount: Double,
+    @ColumnInfo(name = "currentProgress") val currentProgress: Double = 0.0,
+    @ColumnInfo(name = "deadline") val deadline: Long? = null,
+
+    // Fields for No Spending Streaks
+    @ColumnInfo(name = "categoryId") val categoryId: Int?, // The category to avoid spending in
+    @ColumnInfo(name = "streakStartDate") val streakStartDate: Long?, // Timestamp for the streak start
+    @ColumnInfo(name = "currentStreakDays") val currentStreakDays: Int = 0, // Number of days in the current streak
+    @ColumnInfo(name = "longestStreakDays") val longestStreakDays: Int = 0 // User's longest streak
+)
---
+++ app/src/main/java/com/example/pennywise/data/model/User.kt
@@ -0,0 +1,27 @@
+package com.example.pennywise.data.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.sql.Date
+
+@Entity(
+    tableName = "users",
+    foreignKeys = [ForeignKey(
+        entity = Wallet::class,
+        parentColumns = ["walletId"],
+        childColumns = ["walletId"],
+        onDelete = ForeignKey.CASCADE // Deletes the user if the wallet is deleted
+    )]
+)
+data class User(
+    @PrimaryKey(autoGenerate = true) val userId: Int = 0,
+    @ColumnInfo(name = "firstname") val firstname: String?,
+    @ColumnInfo(name = "lastname") val lastname: String?,
+    @ColumnInfo(name = "email") val email: String,
+    @ColumnInfo(name = "password") val password: String,
+    @ColumnInfo(name = "dob") val dateOfBirth: Date,
+    @ColumnInfo(name = "profilePicture") val profilePicture: String? = null,
+    @ColumnInfo(name = "walletId") val walletId: Int // Foreign key referencing Wallet.walletId
+)
